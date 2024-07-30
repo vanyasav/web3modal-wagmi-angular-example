870f03e53fca9bd43a147a099cba64c67697ffc9
vanyasav <i.savilov@greengreystudio.com>
2024-07-30 14:30:49 +0200

    add web3modal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
 import { Component } from '@angular/core';
+import {
+  createWeb3Modal,
+  defaultWagmiConfig,
+  Web3Modal,
+} from '@web3modal/wagmi';
+import { mainnet } from '@wagmi/core/chains';
+
+//INSERT WALLETCONNECT PROJECT ID
+const WALLET_CONNECT_PROJECT_ID = '';
+
+const config = defaultWagmiConfig({
+  chains: [mainnet],
+  projectId: WALLET_CONNECT_PROJECT_ID,
+  metadata: {
+    name: 'Html Example',
+    description: 'Html Example',
+    url: 'https://web3modal.com',
+    icons: ['https://avatars.githubusercontent.com/u/37784886'],
+  },
+  enableInjected: true,
+  enableWalletConnect: true,
+});
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'WalletConnect';
+  modal: Web3Modal;
+  constructor() {
+    {
+      this.modal = createWeb3Modal({
+        wagmiConfig: config,
+        projectId: WALLET_CONNECT_PROJECT_ID,
+        themeMode: 'light',
+      });
+    }
+  }
+
+  connectWallet() {
+    this.modal.open();
+  }
 }
